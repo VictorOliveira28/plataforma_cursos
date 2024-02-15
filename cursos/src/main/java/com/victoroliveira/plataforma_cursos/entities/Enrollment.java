3442af03e9f1c1195b4c4b37643011f721d27467
Victor Oliveira <victor.oliveira20@icloud.com>
2024-02-15 16:31:45 -0300

    Enrollment, EnrollmentPK

---
+++ cursos/src/main/java/com/victoroliveira/plataforma_cursos/entities/Enrollment.java
@@ -0,0 +1,91 @@
+package com.victoroliveira.plataforma_cursos.entities;
+
+import java.time.Instant;
+
+import com.victoroliveira.plataforma_cursos.entities.pk.EnrollmentPK;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+	@EmbeddedId
+	private EnrollmentPK id = new EnrollmentPK();
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant enrollMoment;	
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant refundMoment;
+	private boolean available;
+	private boolean onlyUpdate;
+	
+	public Enrollment() {		
+	}
+
+	public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available,
+			boolean onlyUpdate) {
+		
+		id.setUser(user);
+		id.setOffer(offer);
+		this.enrollMoment = enrollMoment;
+		this.refundMoment = refundMoment;
+		this.available = available;
+		this.onlyUpdate = onlyUpdate;
+	}
+
+	public User getStudent() {
+		return id.getUser();
+	}
+
+	public void setStudent(User user) {
+		id.setUser(user);
+	}
+	
+	public Offer getOffer() {
+		return id.getOffer();
+	}
+	
+	public void setOffer(Offer offer) {
+		id.setOffer(offer);
+	}
+
+	public Instant getEnrollMoment() {
+		return enrollMoment;
+	}
+
+	public void setEnrollMoment(Instant enrollMoment) {
+		this.enrollMoment = enrollMoment;
+	}
+
+	public Instant getRefundMoment() {
+		return refundMoment;
+	}
+
+	public void setRefundMoment(Instant refundMoment) {
+		this.refundMoment = refundMoment;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public boolean isOnlyUpdate() {
+		return onlyUpdate;
+	}
+
+	public void setOnlyUpdate(boolean onlyUpdate) {
+		this.onlyUpdate = onlyUpdate;
+	}
+	
+	
+	
+}
