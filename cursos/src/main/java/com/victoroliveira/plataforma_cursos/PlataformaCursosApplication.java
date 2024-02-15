108a54be35401fa51dfadc84c82487ac67ccd2c4
Victor Oliveira <victor.oliveira20@icloud.com>
2024-02-15 11:16:50 -0300

    User, Role

---
+++ cursos/src/main/java/com/victoroliveira/plataforma_cursos/PlataformaCursosApplication.java
@@ -0,0 +1,13 @@
+package com.victoroliveira.plataforma_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaCursosApplication.class, args);
+	}
+
+}
